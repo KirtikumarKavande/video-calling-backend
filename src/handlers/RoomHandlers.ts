7e3813acb50c4fa158c880e2f46c68a00c032e57
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-14 14:59:26 +0530

    feat:getting userid of joining the room

--- src/handlers/RoomHandlers.ts
+++ src/handlers/RoomHandlers.ts
@@ -2,15 +2,15 @@ import { Socket } from "socket.io";
 import { v4 as uuid } from "uuid";
 
 const roomHandler = (socket: Socket) => {
-  const createRoom = () => {
+  const createRoom = ({userId}) => {
     const roomId = uuid();
     socket.join(roomId);
     socket.emit("room-created", {roomId});
-    console.log("room created with id", roomId);
+    console.log("room created with id", roomId +"joined by user",userId);
   };
 
-  const joinRoom = () => {
-    console.log("joined room")
+  const joinRoom = (data:{useId:string,roomId:string}) => {
+    console.log(`new user with id ${data.useId} joined room ${data.roomId}`);
   };
 
   socket.on("create-room", createRoom);
