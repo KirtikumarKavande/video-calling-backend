dbae5b5c8ac5bef579cc1831e7a0e500ac2b7826
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-16 08:22:26 +0530

    knowing user in room who has joined the room

--- src/handlers/RoomHandlers.ts
+++ src/handlers/RoomHandlers.ts
@@ -3,25 +3,29 @@ import { v4 as uuid } from "uuid";
 const rooms: Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
-
   const createRoom = ({ userId }: { userId: string }) => {
     console.log("userID check", userId);
     const roomId = uuid();
     socket.join(roomId);
+    rooms[roomId] = [];
+
     socket.emit("room-created", { roomId });
     console.log("room created with id", roomId + " joined by user", userId);
   };
 
   const joinRoom = ({ userId, roomId }: { userId: string; roomId: string }) => {
-    if (!rooms[roomId]) {
-      rooms[roomId] = [];
+    if (rooms[roomId]) {
+      rooms[roomId].push(userId);
+      socket.join(roomId);
+       socket.on('ready',()=>{
+        socket.to(roomId).emit("user joined with user Id",userId)
+
+       })
       console.log(`new user with id ${userId} joined room ${roomId}`);
     }
-    rooms[roomId].push(userId);
-  console.log("lets see", rooms);
+    console.log("lets see", rooms);
 
   };
-
 
   socket.on("create-room", createRoom);
   socket.on("join-room", joinRoom);
